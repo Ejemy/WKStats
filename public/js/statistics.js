2ca4b0a4ca5c8d64504f97d34910a8af7f54faf0
Eric Iwan <eric.iwan@proton.me>
2023-06-21 05:38:08 +0000

    more

--- public/js/statistics.js
+++ public/js/statistics.js
@@ -5,51 +5,108 @@ var today = new Date();
 var day = today.getDate() -7;
 var month = today.getMonth();
 var year = today.getFullYear();
-console.log(year, month, day)
 var yes = new Date(year, month, day).toISOString()
 console.log(yes)
 var tokenDiv = document.getElementById("tokenkey");
 var apiToken = tokenDiv.innerText;
-var apiEndpointPath = 'review_statistics?updated_after=' + yes;
+var apiEndpointPath1 = "assignments";
 var requestHeaders =
 new Headers({
     Authorization: 'Bearer ' + apiToken,
 });
-var apiEndpoint =
-new Request('https://api.wanikani.com/v2/' + apiEndpointPath, {
+var apiEndpoint1 =
+new Request('https://api.wanikani.com/v2/' + apiEndpointPath1, {
     method: 'GET',
     headers: requestHeaders
 });
+var apiEndpointPath2 = "review_statistics";
+var requestHeaders =
+new Headers({
+    Authorization: 'Bearer ' + apiToken,
+});
+var apiEndpoint2 =
+new Request('https://api.wanikani.com/v2/' + apiEndpointPath2, {
+    method: 'GET',
+    headers: requestHeaders
+});
+const assignments = fetch(apiEndpoint1)
+.then(response => response.json());
+const subjects = fetch(apiEndpoint2)
+.then(response => response.json());
 
-fetch(apiEndpoint)
-.then(response => response.json())
-.then((responseBody) => {
-    const datum = responseBody.data;
-    let wkdata = []
-    for(var i in datum){
-        wkdata[i] = [];
-        for(var j in datum[i]){
-            wkdata[i].push(datum[i][j])
+
+Promise.all([assignments, subjects])
+.then(([data1, data2]) => {
+    combinedData = [];
+    for(var i in data1.data){
+        combinedData[i] = {}
+        for(var j in data2.data){
+            if(data1.data[i].data.subject_id == data2.data[j].data.subject_id){
+                combinedData[i].subject_id = data1.data[i].data.subject_id;
+                combinedData[i].burned_at = data1.data[i].data.burned_at;
+                combinedData[i].subject_type = data1.data[i].data.subject_type;
+                combinedData[i].percentage_correct = data2.data[j].data.percentage_correct;
+            }
         }
-        wkdata.push(datum[i].data)
     }
-    console.log(wkdata)
+    
+  
+    
+    
+    console.log(combinedData)
+
+    const w = 80;
+    const h = 40;
+    const padding = 1;
+    const colors = [
+        "#e60049",
+        "#0bb4ff",
+        "#50e991",
+        "#e6d800",
+        "#9b19f5",
+        "#ffa300",
+        "#dc0ab4"
+      ];
+
     const svg = d3
     .select("body")
     .append("svg")
-    .attr("height", 500)
-    .attr("width", 500)
+    .attr("height", h + padding + "vh")
+    .attr("width", w + padding + "vw")
     .style("box-shadow", "1px 5px 10px black");
+    
+    
+    //var root = d3.hierarchy(wkdata).sum((d) => d.data);
+    //d3.treemap().size([w, h]).padding(2)(root);
+    const yMax = d3.max(combinedData, d => d.percentage_correct)
+    const yMin = d3.min(combinedData, d => d.percentage_correct)
+    const xScale = d3
+    .scaleLinear()
+    .domain([0, combinedData.length])
+    .range([padding + "vw", w - padding + "vw"]);
+    const yScale = d3
+    .scaleLinear()
+    .domain([yMin, yMax])
+    .range([h - padding, padding])
+    
     svg
     .selectAll("rect")
-    .data(wkdata)
+    .data(combinedData)
     .enter()
+    .append("rect")
     .attr("width", 3)
-    .attr("height", 3)
-    .attr("x", (d, i)=> i+3)
-    .attr("y", 0)
-    .style("fill", "blue")
+    .attr("height", (d,i)=> h - yScale(d.percentage_correct) - padding + "vh")
+    .attr("x", (d, i)=> xScale(i))
+    .attr("y", d => yScale(d.percentage_correct) +  "vh")
+    .style("fill", function(d){
+        if(d.burned_at == null){
+            return "red"
+        } else {
+            return "blue"
+        }
+    })
 })
+
 
 
 
